f4b659b1db47fa8d8fa01309c121b49f1d9cfaaf
Zamy17 <137757323+Zamy17@users.noreply.github.com>
2023-10-19 12:48:50 +0700

    Add files via upload

---
+++ LATIHAN 1.1.php
@@ -0,0 +1,24 @@
+<?php
+    $x = 10;
+    
+    echo " Aku adalah angka 10 </br>";
+     //perkalian
+     $hasil_jumlahkali = $x * 8;
+    echo " Jika aku dikali 8, jumlahku sekarang $hasil_jumlahkali </br>";
+
+    //pembagian
+    $hasil_jumlahbagi = $hasil_jumlahkali / 4;
+    echo " Jika aku dibagi 4, jumlahku sekarang $hasil_jumlahbagi </br>";
+
+
+    //pengurangan
+    $hasil_jumlahkurang = $hasil_jumlahbagi - 6;
+    echo " Jika aku dikurang 6, jumlahku sekarang $hasil_jumlahkurang </br>";
+
+
+    //penjumlahan
+    $hasil_jumlah = $hasil_jumlahkurang +2;
+    echo " Jika aku ditambah 2, jumlahku sekarang $hasil_jumlah </br>";
+
+
+?>
